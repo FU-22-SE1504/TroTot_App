7d8a0fc2d2995ac725fb6147332650fa7fd74b1e
wibulord <namdanglethanh@gmail.com>
2022-10-29 14:24:01 +0700

    Revert "Revert "Add And Homepage""
    This reverts commit 60745c1c1da11ad636063b2b6ba6676fd12e60aa.

--- app/src/main/java/com/example/trotot/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/trotot/Fragment/HomeFragment.java
@@ -4,20 +4,13 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.example.trotot.Database.ConnectDatabase;
 import com.example.trotot.R;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 /**
  * A simple {@link Fragment} subclass.
